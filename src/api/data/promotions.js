205bdd5db8e4608c30a694a43785387793252ce2
charleshugochg <minheinkhant@hotmail.com>
2021-06-06 17:26:54 +0630

    promotions mock data added

--- src/api/data/promotions.js
+++ src/api/data/promotions.js
@@ -4,23 +4,23 @@ export default [
         image: "https://ae01.alicdn.com/kf/Ha5bbfb3a035e49618d606461a83a975ad.png_.webp"
     },
     {
-        title: "",
-        image: ""
+        title: "Toy & Novelty figures",
+        image: "https://ae01.alicdn.com/kf/Hec34595ec984407486e2f1fe12868632x.png_.webp"
     },
     {
-        title: "",
-        image: ""
+        title: "Bathroom renovation ideas",
+        image: "https://ae01.alicdn.com/kf/Ha8fa9a3d16fb4f86b600a3298043c970a.png_.webp"
     },
     {
-        title: "",
-        image: ""
+        title: "Cool picks for summer",
+        image: "https://ae01.alicdn.com/kf/Hc4d8ece1a64e44ec8b4016cdf18741a1q.png_.webp"
     },
     {
-        title: "",
-        image: ""
+        title: "Fresh finds",
+        image: "https://ae01.alicdn.com/kf/H293b6d59b4144b3eb8bc4f3d0a4c73514.png_.webp"
     },
     {
-        title: "",
-        image: ""
+        title: "Discover what's trending",
+        image: "https://ae01.alicdn.com/kf/H0410ba549f82443bb9b7c55dac425418y.png_.webp"
     },
 ]
