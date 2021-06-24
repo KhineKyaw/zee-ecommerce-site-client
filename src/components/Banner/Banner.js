f9630ad871082e9570d88a9acbc68a1aa72a4492
charleshugochg <minheinkhant@hotmail.com>
2021-06-24 10:25:13 +0630

    style calc

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 
 import Card from '../UI/Card/Card'
 
@@ -6,27 +6,58 @@ import classes from "./Banner.module.css"
 
 import { promotions } from '../../api/data'
 
+const minRoundDistance = (i, t, r) => {
+  const dd = t - i
+  let rd;
+  if (dd < 0) {
+    rd = dd + r
+  } else {
+    rd = dd - r
+  }
+  if (Math.abs(dd) > Math.abs(rd)) {
+    return rd
+  } else {
+    return dd
+  }
+}
+
+const calcLeft = (offset) => {
+  if (Math.abs(offset) > 2) {
+    return '50%'
+  }
+  return `${50 + (offset * 7)}%`
+}
+
+const calcZIndex = (offset) => {
+  return 3 - Math.abs(offset)
+}
+
+const calcScale = (offset) => {
+  return 1 - (0.1 * Math.abs(offset))
+}
+
 const Banner = () => {
+  const [index, setIndex] = useState(0)
+
+  const slideStyle = (i) => {
+    const offset = minRoundDistance(i, index, promotions.length)
+    const left = calcLeft(offset)
+    const zIndex = calcZIndex(offset)
+    const scale = calcScale(offset)
+    return {
+      left: left,
+      zIndex: zIndex,
+      transform: `translate(-50%) scale(${scale})`
+    }
+  }
+
   return (
     <div className={classes["banner-container"]}>
-      <Card className={classes["slide"]} style={{left: '50%', zIndex: 3, transform: 'translateX(-50%) scale(1)'}}>
-        <img src={promotions[0].image} alt={promotions[0].title}/>
-      </Card>
-      <Card className={classes["slide"]} style={{left: '43%', zIndex: 2, transform: 'translateX(-50%) scale(0.9)'}}>
-        <img src={promotions[1].image} alt={promotions[1].title}/>
-      </Card>
-      <Card className={classes["slide"]} style={{left: '57%', zIndex: 2, transform: 'translateX(-50%) scale(0.9)'}}>
-        <img src={promotions[2].image} alt={promotions[2].title}/>
-      </Card>
-      <Card className={classes["slide"]} style={{left: '36%', zIndex: 1, transform: 'translateX(-50%) scale(0.8)'}}>
-        <img src={promotions[3].image} alt={promotions[3].title}/>
-      </Card>
-      <Card className={classes["slide"]} style={{left: '64%', zIndex: 1, transform: 'translateX(-50%) scale(0.8)'}}>
-        <img src={promotions[4].image} alt={promotions[4].title}/>
-      </Card>
-      <Card className={classes["slide"]} style={{left: '50%', zIndex: 0, transform: 'translateX(-50%) scale(0)'}}>
-        <img src={promotions[4].image} alt={promotions[4].title}/>
-      </Card>
+      {promotions.map((p, i) => (
+        <Card className={classes["slide"]} style={slideStyle(i)}>
+          <img src={p.image} alt={p.title} />
+        </Card>
+      ))}
       <div className={classes.prev} onClick={() => null}>&#10094;</div>
       <div className={classes.next} onClick={() => null}>&#10095;</div>
     </div>
