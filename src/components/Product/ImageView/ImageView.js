4b97d23f5ea822e502a8d3e1864acff48190cdf7
DanielKhine <danielrio126@gmail.com>
2021-06-25 12:45:48 +0630

    added magnifying function, image view.

--- src/components/Product/ImageView/ImageView.js
+++ src/components/Product/ImageView/ImageView.js
@@ -4,15 +4,45 @@ import classes from "./ImageView.module.css"
 
 const ImageView = props => {
   const [selectedImageIndex, selectImageindex] = useState(0)
+  const [imgPos, setImgPos] = useState({ x: 0, y: 0 })
+  const [scaleImage, setScaleImage] = useState({ x: 1, y: 1 })
 
   const onSelect = id => () => {
     selectImageindex(id)
   }
 
+  const onImageZoom = e => {
+    var rect = e.target.getBoundingClientRect()
+    var x = e.clientX - rect.left
+    var y = e.clientY - rect.top
+    setImgPos({ x: x, y: y })
+  }
+
+  const onMouseLeave = () => {
+    setImgPos({ x: 0, y: 0 })
+    setScaleImage({ x: 1, y: 1 })
+  }
+
+  const onMouseEnter = () => {
+    setImgPos({ x: 0, y: 0 })
+    setScaleImage({ x: 1.46, y: 1.46 })
+  }
+
   return (
     <div className={classes.view}>
       <div className={classes.imageMagnifier}>
-        <img src={props.data[selectedImageIndex]} />
+        <img
+          style={{
+            transformOrigin: `${imgPos.x}px ${imgPos.y}px`,
+            transform: `scale(${scaleImage.x}, ${scaleImage.y})`
+          }}
+          src={props.data[selectedImageIndex]}
+        />
+        <div
+          className={classes.eventListenerDiv}
+          onMouseMove={onImageZoom}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}></div>
       </div>
       <ul className={classes.imagesWrapper}>
         {props.data.map((item, id) => (
