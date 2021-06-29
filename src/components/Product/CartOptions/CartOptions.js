e2afc6c38af74496359687150a2ac5e7d35651f1
charleshugochg <minheinkhant@hotmail.com>
2021-06-29 15:10:53 +0630

    layout rough final

--- src/components/Product/CartOptions/CartOptions.js
+++ src/components/Product/CartOptions/CartOptions.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import Button from '../../UI/Button/Button'
+import Card from '../../UI/Card/Card'
 
 import classes from "./CartOptions.module.css"
 
@@ -19,41 +20,42 @@ const CartOptions = props => {
         </div>
         <span><a href="#reviews">10 reviews</a></span>
       </div>
+      <div className={classes.splitter}></div>
       <div className={classes.option_container}>
         <div className={classes.option}>
-          <h5 className={classes.option_name}>Color</h5>
+          <span className={classes.option_name}>Color:</span>
+          <span className={classes.option_item_value}>{data.items[0].options[0].value}</span>
           <div className={classes.option_item_container}>
-            <div className={classes.option_item}>
+            <Card className={`${classes.option_item} ${classes.selected}`}>
               <img className={classes.option_item_image} src={data.items[0].options[0].image}></img>
-              <span className={classes.option_item_value}>{data.items[0].options[0].value}</span>
-            </div>
-            <div className={classes.option_item}>
+            </Card>
+            <Card className={classes.option_item}>
               <img className={classes.option_item_image} src={data.items[1].options[0].image}></img>
-              <span className={classes.option_item_value}>{data.items[1].options[0].value}</span>
-            </div>
+            </Card>
           </div>
         </div>
         <div className={classes.option}>
-          <h5 className={classes.option_name}>Size</h5>
+          <span className={classes.option_name}>Size:</span>
+          <span className={classes.option_item_value}>{data.items[0].options[1].value}</span>
           <div className={classes.option_item_container}>
-            <div className={classes.option_item}>
-              <div className={classes.option_item_value_box}>
-                {data.items[0].options[1].value}
-              </div>
-            </div>
-            <div className={classes.option_item}>
-              <span className={classes.option_item_value_box}>{data.items[1].options[1].value}</span>
-            </div>
+            <Card className={`${classes.option_item} ${classes.selected}`}>
+              {data.items[0].options[1].value[0]}
+            </Card>
+            <Card className={classes.option_item}>
+              {data.items[1].options[1].value[0]}
+            </Card>
           </div>
         </div>
       </div>
+      <div className={classes.splitter}></div>
       <h1 className={classes.price}>Ks {`19,000`}</h1>
+      <div className={classes.splitter}></div>
       <div className={classes.quantity_container}>
         <span className={classes.quantity_label}>Quantity</span>
         <div className={classes.quantity_controller}>
-          <ion-icon name="remove-circle"></ion-icon>
+          <ion-icon name="remove"></ion-icon>
           <span className={classes.quantity_value}>1</span>
-          <ion-icon name="add-circle"></ion-icon>
+          <ion-icon name="add"></ion-icon>
         </div>
         <span className={classes.quantity_left}>{10} items left</span>
       </div>
