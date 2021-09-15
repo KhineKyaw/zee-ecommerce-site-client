850028404def354cd2496b4425896ca2e1021d3a
charleshugochg <minheinkhant@hotmail.com>
2021-09-15 16:47:45 +0630

    link added

--- src/components/Navigations/Categories1/Categories.js
+++ src/components/Navigations/Categories1/Categories.js
@@ -1,27 +1,32 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
-import classes from "./Categories.module.css"
-import getCategories from "../../../api/getCategories"
+import classes from "./Categories.module.css";
+import getCategories from "../../../api/getCategories";
 
 const Categories = () => {
-  const [categories, setCategories] = useState()
+  const [categories, setCategories] = useState();
 
   useEffect(() => {
-    setCategories(getCategories())
-  }, [])
+    setCategories(getCategories());
+  }, []);
 
   const CategoriesList = categories
-    ? categories.map(item => {
-        return <p key={item.id}>{item.name}</p>
+    ? categories.map((item) => {
+        return (
+          <Link to={"/products?categoryId=" + item.id} key={item.id}>
+            <p>{item.name}</p>
+          </Link>
+        );
       })
-    : "Loading"
+    : "Loading";
 
   return (
     <div className={classes.categories}>
       <h5>Categories</h5>
       {CategoriesList}
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
