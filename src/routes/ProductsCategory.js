4558f9eb1c68254181ac8a0f5b24cc9c67a508e5
charleshugochg <minheinkhant@hotmail.com>
2021-09-15 16:54:01 +0630

    home route added

--- src/routes/ProductsCategory.js
+++ src/routes/ProductsCategory.js
@@ -26,7 +26,7 @@ const ProductsCategory = () => {
 
   return (
     <Layout>
-      <Breadcrumb links={{ Home: "Home" }} last={"Filter"} />
+      <Breadcrumb links={{ Home: "home" }} last={"Filter"} />
       <div className={classes.body}>
         <StickyContainer>
           <Categories />
