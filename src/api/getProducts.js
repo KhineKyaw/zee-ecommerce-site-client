7780cf368e869f9f4ccd5d4fc4f72baae4f48382
charleshugochg <minheinkhant@hotmail.com>
2021-09-16 16:12:17 +0630

    fixed

--- src/api/getProducts.js
+++ src/api/getProducts.js
@@ -4,18 +4,23 @@ import { shuffle } from "../utils";
 const getAllProducts = () => {
   let fake_products = [];
   for (let i = 0; i < 10; i++) {
+    const offset = fake_products.length;
     shuffle(
       products.map((pd, id) => {
         return {
           ...pd,
-          id: fake_products.length + id,
           title: pd.title,
           imageUrl: pd.images[0],
           price: pd.items[0].price,
         };
       }),
       i
-    ).forEach((p) => fake_products.push(p));
+    ).forEach((p, i) =>
+      fake_products.push({
+        ...p,
+        id: offset + i,
+      })
+    );
   }
   return fake_products;
 };
@@ -23,8 +28,9 @@ const getAllProducts = () => {
 const getProducts = (start = 0, count = 10, category_id) => {
   let result = getAllProducts();
   const length = result.length;
+  category_id = Number(category_id);
   if (category_id)
-    result = result.filter(({ categoryId }) => category_id == categoryId);
+    result = result.filter(({ categoryId }) => category_id === categoryId);
   if (start < 0) {
     result = result.reverse();
     start = -start - 1;
