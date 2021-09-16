5d7acd737930910cff0df6560bdbd60d5cdc95f4
charleshugochg <minheinkhant@hotmail.com>
2021-09-16 15:17:37 +0630

    getProduct fixed

--- src/api/getProducts.js
+++ src/api/getProducts.js
@@ -1,46 +1,44 @@
-import { products } from "./data"
-import { shuffle } from "../utils"
+import { products } from "./data";
+import { shuffle } from "../utils";
 
 const getAllProducts = () => {
-  let fake_products = []
+  let fake_products = [];
   for (let i = 0; i < 10; i++) {
-    fake_products.push(
-      shuffle(
-        products.map((pd, id) => {
-          return {
-            ...pd,
-            id: fake_products.length + id,
-            title: pd.title,
-            imageUrl: pd.images[0],
-            price: pd.items[0].price
-          }
-        }),
-        i
-      )
-    )
+    shuffle(
+      products.map((pd, id) => {
+        return {
+          ...pd,
+          id: fake_products.length + id,
+          title: pd.title,
+          imageUrl: pd.images[0],
+          price: pd.items[0].price,
+        };
+      }),
+      i
+    ).forEach((p) => fake_products.push(p));
   }
-  return fake_products
-}
+  return fake_products;
+};
 
 const getProducts = (start = 0, count = 10, category_id) => {
-  let result = getAllProducts()
-  const length = result.length
+  let result = getAllProducts();
+  const length = result.length;
   if (category_id)
-    result = result.filter(({ categoryId }) => category_id === categoryId)
+    result = result.filter(({ categoryId }) => category_id == categoryId);
   if (start < 0) {
-    result = result.reverse()
-    start = -start - 1
-    result = result.slice(start, start + count)
-    result = result.reverse()
+    result = result.reverse();
+    start = -start - 1;
+    result = result.slice(start, start + count);
+    result = result.reverse();
   } else {
-    result = result.slice(start, start + count)
+    result = result.slice(start, start + count);
   }
-  const nextId = result.length && start + count
+  const nextId = result.length && start + count;
   return {
     result,
     nextId,
-    length
-  }
-}
+    length,
+  };
+};
 
-export default getProducts
+export default getProducts;
