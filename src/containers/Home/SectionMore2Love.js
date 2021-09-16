14ddbc22edb7bbef0d0058f8be50adf56ffe355d
charleshugochg <minheinkhant@hotmail.com>
2021-09-16 15:23:22 +0630

    fixes:

--- src/containers/Home/SectionMore2Love.js
+++ src/containers/Home/SectionMore2Love.js
@@ -1,38 +1,38 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useContext, useState, useEffect } from "react";
 
-import ProductsSectionBar from "../../components/Product/ProductsSectionBar/ProductsSectionBar"
+import ProductsSectionBar from "../../components/Product/ProductsSectionBar/ProductsSectionBar";
 // import ListView from "../../components/UI/ListView/ListView"
 // import ProductItem from "../../components/Product/ProductItem/ProductItem"
-import ProductItem from "../../components/Product/ProductItem/ProductItem"
+import ProductItem from "../../components/Product/ProductItem/ProductItem";
 
-import getAllProducts from "../../api/getProducts"
-import LanguageContext from "../../context/language-context"
-import GridView from "../../components/UI/GridView/GridView"
-import classes from "./SectionMore2Love.module.css"
+import getProducts from "../../api/getProducts";
+import LanguageContext from "../../context/language-context";
+import GridView from "../../components/UI/GridView/GridView";
+import classes from "./SectionMore2Love.module.css";
 
 const SectionMore2Love = () => {
-  const { languageDict: texts } = useContext(LanguageContext)
-  const [activePage, setActivePage] = useState(1)
-  const [data, setData] = useState()
+  const { languageDict: texts } = useContext(LanguageContext);
+  const [activePage, setActivePage] = useState(1);
+  const [data, setData] = useState();
 
-  const loadMoreHandler = props => {
-    setActivePage(prev => prev + 1)
-  }
+  const loadMoreHandler = (props) => {
+    setActivePage((prev) => prev + 1);
+  };
 
   useEffect(() => {
-    setData(getAllProducts().result[0])
-  }, [])
+    setData(getProducts().result);
+  }, []);
 
   return (
     <>
       <ProductsSectionBar
         className={classes.margin}
-        type='mid'
+        type="mid"
         title={texts.home["section-title-3"]}
       />
       <GridView data={data} renderItem={ProductItem} />
     </>
-  )
-}
+  );
+};
 
-export default SectionMore2Love
+export default SectionMore2Love;
