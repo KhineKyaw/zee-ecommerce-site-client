cf661409ea327102e79b930dcf189309ecd07da4
DanielKhine <danielrio126@gmail.com>
2021-09-23 17:31:13 +0630

    added image-loader to store items.

--- src/components/Stores/StoreProfileItem/StoreProfileItem.js
+++ src/components/Stores/StoreProfileItem/StoreProfileItem.js
@@ -3,19 +3,25 @@ import React from "react"
 import classes from "./StoreProfileItem.module.css"
 import Card from "../../UI/Card/Card"
 import { Link } from "react-router-dom"
+import ImageLoader from "../../UI/ImageLoader/ImageLoader"
 
 const StoreProfileItem = props => {
   const { item } = props
+
   return (
     <Link to={`/store/${item.id}`}>
       <Card className={classes.card}>
         <div className={classes.item}>
-          <div
+          <ImageLoader
             className={classes.background}
-            style={{
-              backgroundImage: `url('${item.background}')`
-            }}></div>
-          <img src={item.logo} alt={item.name + " logo"} />
+            src={item.background}
+            alt={item.name + " background"}
+          />
+          <ImageLoader
+            className={classes.logo}
+            src={item.logo}
+            alt={item.name + " logo"}
+          />
           <p>{item.name}</p>
         </div>
       </Card>
